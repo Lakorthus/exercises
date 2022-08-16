047724082fe95f3b19b7fab851109015420e3743
Lakorthus <julio28ve@gmail.com>
2022-08-16 16:48:56 +0100

    color render and  darkmode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ColorRenderer from "./01-color-render/colorRender";
+import DarkMode from "./02-dark-mode/darkmode";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ColorRenderer />
+      <DarkMode  />
     </div>
+
   );
 }
 
