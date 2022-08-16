e1b1eea7c6d822678daea1033d920bebc76f3dc6
Lakorthus <julio28ve@gmail.com>
2022-08-16 17:24:12 +0100

    dog app  and score keeper

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import ColorRenderer from "./01-color-render/colorRender";
 import DarkMode from "./02-dark-mode/darkmode";
 import FormValidator from "./03-form-validator/form";
+import DogPics from "./04-dog-picture/dogpic";
+import ScoreKeeper from "./05-score-keeping/scorekeeper";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <ColorRenderer />
       <DarkMode  />
       <FormValidator />
+      <DogPics/>
+      <ScoreKeeper/>
     </div>
 
   );
