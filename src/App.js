22e0003fbdd5a448c4914ad2a8e7e8b8064cb2c2
Lakorthus <julio28ve@gmail.com>
2022-08-16 16:56:02 +0100

    Form Validator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import ColorRenderer from "./01-color-render/colorRender";
 import DarkMode from "./02-dark-mode/darkmode";
+import FormValidator from "./03-form-validator/form";
 
 function App() {
   return (
     <div className="App">
       <ColorRenderer />
       <DarkMode  />
+      <FormValidator />
     </div>
 
   );
